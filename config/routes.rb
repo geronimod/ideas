28afb3d75c8b89dfa6050060321b83d3442c9148
Geronimo Diaz <geronimod@gmail.com>
2013-02-19 10:55:17 -0300

    Added home controller and fix the yield call in layout

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Ideas::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'ideas#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 

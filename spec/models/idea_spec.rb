506d596f97d4e884c2a48680d703ceafb2655d9d
Goni <gpeleritti@devspark.com>
2013-03-01 11:46:43 -0300

    Adding unit test to Idea Model

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -6,7 +6,15 @@ describe Idea do
     idea = Idea.new
     # debugger
     expect(idea.save).to eq(false)
-    expect(idea.errors.size).to eq(1)
+    #expect(idea.errors.size).to eq(1)
     expect(idea.errors[:content]).to eq(["can't be blank"])
+  end
+  
+  it "validate the user asociate to the idea" do
+    user = User.create email:"pepe@mail.com", password:"1234" 
+    idea = Idea.new content:"idea" 
+    idea.user = user
+    
+    expect(idea.user).to eq(user)
   end
 end
